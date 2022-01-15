ee918127a9f2e31f13337ca5fa00fb1ef6bce5d8
Karan Pratap Singh <synester.gates4@gmail.com>
2022-01-15 16:15:19 +0530

    feat: multi symbols

--- main.go
+++ main.go
@@ -10,13 +10,17 @@ import (
 	"github.com/rs/zerolog/log"
 )
 
+// TODO: impl. DCA
+
 var key string = os.Getenv("BINANCE_API_KEY")
 var secret string = os.Getenv("BINANCE_SECRET")
 
-// TODO: impl. DCA
-// TODO: impl. RSI
+var interval string = "1m"
 
-var symbol = "ETHUSDT"
+var symbols = []string{
+	"BTCUSDT",
+	"ETHUSDT",
+}
 
 func init() {
 	InitLogger()
@@ -26,9 +30,7 @@ func main() {
 	forever := make(chan bool)
 
 	bus := events.NewEventBus()
-
 	bex := exchange.NewBinance(key, secret, bus, false)
-	go bex.Kline(symbol, "1m")
 
 	config := strategy.RsiConfig{
 		Overbought: 60,
@@ -37,8 +39,24 @@ func main() {
 
 	rsi := strategy.NewRsi("RSI_60-40", config, bus)
 
+	for _, symbol := range symbols {
+		go bex.Kline(symbol, interval)
+	}
+
 	bus.Subscribe(events.Kline, func(p events.KlinePayload) {
 		rsi.Predict(p.Kline, p.Symbol)
+	})
+
+	bus.Subscribe(events.SignalBuy, func(p events.SignalBuyPayload) {
+		log.Info().Str("symbol", p.Symbol).Msg(events.SignalBuy)
+		// TODO: send notification
+		bex.Buy(p.Symbol)
+	})
+
+	bus.Subscribe(events.SignalSell, func(p events.SignalSellPayload) {
+		log.Info().Str("symbol", p.Symbol).Msg(events.SignalSell)
+		// TODO: send notification
+		bex.Sell(p.Symbol)
 	})
 
 	<-forever
