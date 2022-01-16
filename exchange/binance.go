40a650e4bd6b913b8031a866c8652e334fb4c29d
Karan Pratap Singh <synester.gates4@gmail.com>
2022-01-16 19:42:08 +0530

    refactor: binance

--- exchange/binance.go
+++ exchange/binance.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"fmt"
 	"strconv"
+	"strings"
 	"trader/events"
 
 	"trader/types"
@@ -11,6 +12,8 @@ import (
 	"github.com/adshao/go-binance/v2"
 	"github.com/rs/zerolog/log"
 )
+
+var ZeroBalance = 0.00000000
 
 type Binance struct {
 	client *binance.Client
@@ -27,32 +30,46 @@ func NewBinance(key, secret string, pubsub events.PubSub, test bool) Binance {
 	return Binance{client, pubsub, test}
 }
 
-func (b Binance) PrintUserInfo() {
+func (b Binance) GetAccount() *binance.Account {
 	svc := b.client.NewGetAccountService()
-	user, err := svc.Do(context.Background())
+	account, err := svc.Do(context.Background())
 
 	if err != nil {
 		log.Error().Err(err).Msg("Binance.UserInfo")
 	}
 
-	fmt.Println("---- User Info ----")
-	fmt.Println("Account Type:", user.AccountType)
-	fmt.Println("Can Trade:", user.CanTrade)
-	fmt.Println("Available Balance")
+	return account
+}
 
-	for _, balance := range user.Balances {
+func (b Binance) PrintAccountInfo() {
+	acc := b.GetAccount()
+
+	fmt.Println("-------- Account Info --------")
+	fmt.Println("Type:", acc.AccountType)
+	fmt.Println("Can Trade:", acc.CanTrade)
+	fmt.Println(b.StringifyBalance(acc.Balances))
+	fmt.Println("------------------------------")
+}
+
+func (Binance) StringifyBalance(userBalances []binance.Balance) string {
+	var balances = []string{
+		"Balance:",
+	}
+
+	for _, balance := range userBalances {
 		amt, err := strconv.ParseFloat(balance.Free, 64)
 
 		if err != nil {
 			log.Error().Err(err).Msg("Binance.ParsingBalance")
 		}
 
-		if amt > 0.00000000 {
-			fmt.Println(" -", balance.Asset, balance.Free)
+		if amt > ZeroBalance {
+			b := fmt.Sprintf(" - %v %v", balance.Asset, balance.Free)
+			balances = append(balances, b)
 		}
 	}
 
-	fmt.Println("-------------------")
+	return strings.Join(balances, "\n")
 }
 
 func (b Binance) Buy(symbol string) {
@@ -92,6 +109,8 @@ func (b Binance) Kline(symbol string, interval string) {
 
 	errHandler := func(err error) {
 		log.Error().Err(err).Msg("Binance.KLine")
+		// Try to restart
+		b.Kline(symbol, interval)
 	}
 
 	binance.WsKlineServe(symbol, interval, wsKlineHandler, errHandler)
