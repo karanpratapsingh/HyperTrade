cdfab108d13a19a73c2026cf25b37d08eb11ca73
Karan Pratap Singh <synester.gates4@gmail.com>
2022-01-23 22:05:58 +0530

    feat(model): configs

---
+++ services/exchange/db/configs.model.go
@@ -0,0 +1,42 @@
+package db
+
+import (
+	"errors"
+
+	"github.com/rs/zerolog/log"
+	"gorm.io/gorm"
+)
+
+type Configs struct {
+	Symbol        string  `gorm:"primaryKey"`
+	Minimum       float64 `gorm:"not null"`
+	AllowedAmount float64 `gorm:"not null"`
+}
+
+func (db DB) GetConfig(symbol string) Configs {
+	var config Configs
+
+	result := db.conn.First(&config, "symbol = ?", symbol)
+
+	if result.Error != nil && !errors.Is(result.Error, gorm.ErrRecordNotFound) {
+		log.Error().Err(result.Error).Str("symbol", symbol).Msg("DB.Positions.GetPosition")
+	}
+
+	return config
+}
+
+func (db DB) CreateConfig(symbol string, minimum, allowedAmt float64) error {
+	config := Configs{
+		Symbol:        symbol,
+		Minimum:       minimum,
+		AllowedAmount: allowedAmt,
+	}
+
+	result := db.conn.Create(&config)
+
+	if result.Error != nil {
+		log.Error().Err(result.Error).Msg("DB.Config.CreateConfig")
+	}
+
+	return result.Error
+}
