05a42b5415b1c47f349d00ac6214b974c16605a9
Karan Pratap Singh <synester.gates4@gmail.com>
2022-01-23 21:04:36 +0530

    feat: trade exec

--- services/exchange/main.go
+++ services/exchange/main.go
@@ -1,11 +1,12 @@
 package main
 
 import (
-	"fmt"
 	"os"
 
+	"exchange/db"
 	"exchange/internal"
 
+	"github.com/adshao/go-binance/v2"
 	"github.com/rs/zerolog"
 	"github.com/rs/zerolog/log"
 )
@@ -19,6 +20,8 @@ var interval string = "1m"
 var symbol = "ETHUSDT"
 
 func main() {
+	DB := db.New()
+
 	wait := make(chan bool)
 	env := internal.GetEnv()
 
@@ -31,8 +34,57 @@ func main() {
 	go bex.Kline(symbol, interval)
 
 	pubsub.Subscribe(internal.DataFrameEvent, func(p internal.DataFrameEventPayload) {
-		fmt.Println(p.Trade)
+		ListenTrade(DB, p.Kline, p.Signal)
 	})
 
 	<-wait
+}
+
+// TODO: refactor this
+func ListenTrade(DB db.DB, kline internal.Kline, signal internal.Signal) {
+	side := getSide(signal)
+
+	if side == "" {
+		return
+	}
+
+	log.Trace().Str("symbol", kline.Symbol).Interface("side", side).Msg("Trade.Listen")
+
+	position := DB.GetPosition(kline.Symbol)
+	var holding bool = position.Symbol != ""
+
+	switch side {
+	case binance.SideTypeBuy:
+		if !holding {
+			DB.CreatePosition(kline.Symbol, kline.Close, 0.005) // TODO: get value from db
+			log.Trace().Float64("price", kline.Close).Msg("Trade.Buy.Complete")
+			return
+		}
+		log.Warn().Bool("holding", holding).Msg("Trade.Buy.Skip")
+
+	case binance.SideTypeSell:
+		if holding {
+			DB.DeletePosition(kline.Symbol)
+			log.Trace().Float64("price", kline.Close).Msg("Trade.Sell.Complete")
+			return
+		}
+		log.Warn().Bool("holding", holding).Msg("Trade.Sell.Skip")
+	default:
+	}
+}
+
+func getSide(signal internal.Signal) binance.SideType {
+	var side binance.SideType
+
+	if signal.Buy && !signal.Sell {
+		side = binance.SideTypeBuy
+	} else if signal.Sell && !signal.Buy {
+		side = binance.SideTypeSell
+	}
+
+	return side
+}
+
+func isEmpty(value interface{}) bool {
+	return value == ""
 }
