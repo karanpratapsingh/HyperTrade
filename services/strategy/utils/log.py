dbc4f6409dbc6c731f36da0ae4bef84efc615f07
Karan Pratap Singh <synester.gates4@gmail.com>
2022-01-23 12:52:25 +0530

    update: logs

--- services/strategy/utils/log.py
+++ services/strategy/utils/log.py
@@ -4,7 +4,7 @@ import logging
 class Logger:
     @staticmethod
     def get(name):
-        logging.basicConfig(format='%(levelname)s %(message)s')
+        logging.basicConfig(format='%(name)s:%(levelname)s %(message)s')
         log = logging.getLogger(name)
         log.setLevel(logging.DEBUG)
 
