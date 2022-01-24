1a354c8170810f6473c882847ae81e78c7e188aa
Karan Pratap Singh <synester.gates4@gmail.com>
2022-01-24 14:43:44 +0530

    feat: app.tsx

---
+++ web/src/app.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+function App(): React.ReactElement {
+  return <div>Hello Web!</div>
+}
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+)
