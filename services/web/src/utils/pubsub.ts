259fd025ab705905d1a956df36cf517a50f26eed
Karan Pratap Singh <synester.gates4@gmail.com>
2022-01-28 16:44:52 +0530

    feat(utils): add pubsub

---
+++ services/web/src/utils/pubsub.ts
@@ -0,0 +1,49 @@
+import {
+  Codec,
+  connect,
+  Msg,
+  NatsConnection,
+  NatsError,
+  StringCodec,
+} from 'nats.ws';
+
+const SERVER_URL = 'ws://localhost:8080/nats';
+
+const AUTH = {
+  user: import.meta.env.VITE_NATS_USER,
+  pass: import.meta.env.VITE_NATS_PASS,
+};
+
+export class PubSub {
+  private codec: Codec<string>;
+  private conn: NatsConnection | null = null;
+
+  constructor() {
+    this.codec = StringCodec();
+  }
+
+  init = async (): Promise<void> => {
+    try {
+      this.conn = await connect({
+        servers: SERVER_URL,
+        user: AUTH.user,
+        pass: AUTH.pass,
+      });
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  subscribe = <T>(event: string, cb: (data: T) => void): void => {
+    const callback = (err: NatsError | null, msg: Msg): void => {
+      if (err) {
+        throw err;
+      }
+      const data: T = JSON.parse(this.codec.decode(msg.data));
+      cb(data);
+    };
+
+    this.conn?.subscribe(event, { callback });
+    console.log('Subscribed to:', event);
+  };
+}
