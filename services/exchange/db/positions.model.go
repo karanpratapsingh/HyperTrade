dcfdcb31e9ff1808a2a52ff94840cb60e7c54df5
Karan Pratap Singh <synester.gates4@gmail.com>
2022-01-29 19:39:56 +0530

    update: position structs

--- services/exchange/db/positions.model.go
+++ services/exchange/db/positions.model.go
@@ -9,10 +9,10 @@ import (
 )
 
 type Positions struct {
-	Symbol   string    `gorm:"primaryKey"`
-	Price    float64   `gorm:"not null"`
-	Quantity float64   `gorm:"not null"`
-	Time     time.Time `gorm:"not null"`
+	Symbol   string    `gorm:"primaryKey" json:"symbol"`
+	Price    float64   `gorm:"not null" json:"price"`
+	Quantity float64   `gorm:"not null" json:"quantity"`
+	Time     time.Time `gorm:"not null" json:"time"`
 }
 
 func (db DB) GetPosition(symbol string) Positions {
