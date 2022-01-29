18a508353a604cc9180e60eaede9896638977ae3
Karan Pratap Singh <synester.gates4@gmail.com>
2022-01-29 19:40:04 +0530

    feat: get trades method

--- services/exchange/db/trades.model.go
+++ services/exchange/db/trades.model.go
@@ -7,12 +7,25 @@ import (
 )
 
 type Trades struct {
-	ID       uint      `gorm:"primaryKey;"`
-	Symbol   string    `gorm:"not null"`
-	Entry    float64   `gorm:"not null"`
-	Exit     float64   `gorm:"not null"`
-	Quantity float64   `gorm:"not null"`
-	Time     time.Time `gorm:"not null"`
+	ID       uint      `gorm:"primaryKey;" json:"id"`
+	Symbol   string    `gorm:"not null" json:"symbol"`
+	Entry    float64   `gorm:"not null" json:"entry"`
+	Exit     float64   `gorm:"not null" json:"exit"`
+	Quantity float64   `gorm:"not null" json:"quantity"`
+	Time     time.Time `gorm:"not null" json:"time"`
+}
+
+func (db DB) GetTrades() []Trades {
+	var trades []Trades
+
+	result := db.conn.Find(&trades)
+
+	if result.Error != nil {
+		log.Error().Err(result.Error).
+			Msg("DB.Trades.GetTrades")
+	}
+
+	return trades
 }
 
 func (db DB) CreateTrade(symbol string, entry, exit, quantity float64) Trades {
