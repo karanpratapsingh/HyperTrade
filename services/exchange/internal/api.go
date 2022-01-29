c3422b37b8daa387980995d4d6424fc8756748a5
Karan Pratap Singh <synester.gates4@gmail.com>
2022-01-29 19:11:42 +0530

    feat: balance api

--- services/exchange/internal/api.go
+++ services/exchange/internal/api.go
@@ -1,30 +1,55 @@
 package internal
 
 import (
-	"fmt"
+	"encoding/json"
 	"net/http"
 
 	"github.com/gorilla/mux"
 	"github.com/rs/zerolog/log"
 )
 
-type Api struct{}
+type Api struct {
+	exchange Binance
+}
 
-func NewApi() error {
+func NewApi(exchange Binance) error {
 	port := ":80"
 
 	log.Trace().Str("port", port).Msg("Internal.Api.Init")
 
 	router := mux.NewRouter()
-	api := Api{}
+	api := Api{exchange}
 
-	router.HandleFunc("/", api.healthcheck).Methods(http.MethodGet)
+	router.HandleFunc("/healthz", api.healthcheck).Methods(http.MethodGet)
+	router.HandleFunc("/balance", api.balance).Methods(http.MethodGet)
 
 	err := http.ListenAndServe(port, router)
 	return err
 }
 
+type HealthzResponse struct {
+	Status  int    `json:"status"`
+	Message string `json:"message"`
+}
+
 func (Api) healthcheck(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusOK)
-	fmt.Fprintf(w, "Healthy")
+	response := HealthzResponse{
+		Status:  200,
+		Message: "Healthy",
+	}
+
+	json.NewEncoder(w).Encode(response)
+}
+
+type BalanceResponse struct {
+	Balance []Balance `json:"balance"`
+}
+
+func (a Api) balance(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
+	response := BalanceResponse{
+		Balance: a.exchange.GetBalance(),
+	}
+	json.NewEncoder(w).Encode(response)
 }
