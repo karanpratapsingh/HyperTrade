1b0b8596854c94595618f1af7e22c6b0fa7b1a79
Karan Pratap Singh <synester.gates4@gmail.com>
2022-01-29 22:00:39 +0530

    feat: positions api

---
+++ services/web/src/api/positions.ts
@@ -0,0 +1,29 @@
+import { Result } from './types';
+import axios, { AxiosResponse } from 'axios';
+import { useQuery } from 'react-query';
+
+export type Position = {
+  Symbol: string;
+  Price: number;
+  Quantity: number;
+  time: Date;
+};
+
+type PositionsResponse = {
+  positions: Position[];
+};
+
+export function usePositions(): Result<PositionsResponse> {
+  const fetch = () => axios.get('/exchange/positions');
+  const {
+    data,
+    isLoading: loading,
+    error,
+  } = useQuery<AxiosResponse<PositionsResponse, Error>, Error>(
+    'positions',
+    fetch,
+    { refetchInterval: 2 * 1000 }
+  );
+
+  return { data: data?.data, loading, error };
+}
