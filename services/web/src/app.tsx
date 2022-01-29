8d2a59febe4a5e19c4d58063d5b52e5778eff49d
Karan Pratap Singh <synester.gates4@gmail.com>
2022-01-29 16:14:51 +0530

    feat: app.tsx

--- services/web/src/app.tsx
+++ services/web/src/app.tsx
@@ -3,19 +3,20 @@ import './styles/app.css';
 import React, { useEffect } from 'react';
 import ReactDOM from 'react-dom';
 import { ErrorBoundary } from './components/error-boundary';
-import { KlineChart } from './components/kline-chart';
+import { KlineChart } from './components/charts/kline';
 import { DataFrameEvent, DataFrameEventPayload } from './events';
-import { useKline } from './store/kline';
+import { useDataFrame } from './store/dataframe';
 import { PubSub } from './utils/pubsub';
+import { IndicatorChart } from './components/charts/indicator';
 
 function App(): React.ReactElement {
-  const { add } = useKline();
+  const { add } = useDataFrame();
   async function init() {
     const pubsub = new PubSub();
     await pubsub.init();
 
     pubsub.subscribe<DataFrameEventPayload>(DataFrameEvent, payload => {
-      add(payload.kline);
+      add(payload);
     });
   }
 
@@ -24,8 +25,22 @@ function App(): React.ReactElement {
   }, []);
 
   return (
-    <div className='h-full p-16'>
-      <KlineChart />
+    <div className='h-full p-16 flex flex-1'>
+      <div className='flex flex-col mr-4' style={{ flex: 3 }}>
+        <div>
+          <KlineChart />
+        </div>
+        <div className='flex items-center justify-between mt-4'>
+          <IndicatorChart types={['rsi', 'adx']} />
+          <IndicatorChart types={['macd']} />
+          <IndicatorChart types={['macd_hist', 'macd_signal']} />
+        </div>
+      </div>
+      <div className='flex flex-1 flex-col'>
+        <div className='flex flex-1'>Balance</div>
+        <div className='flex flex-1'>Trades</div>
+        <div className='flex flex-1'>Positions</div>
+      </div>
     </div>
   );
 }
