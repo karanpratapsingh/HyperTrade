8ed8db898e02a1f309e84c8ceafa1965c79e650d
Karan Pratap Singh <synester.gates4@gmail.com>
2022-01-30 15:59:01 +0530

    feat: line colors

--- services/web/src/theme/colors.ts
+++ services/web/src/theme/colors.ts
@@ -1,3 +1,13 @@
+import { Indicators } from '../events/types';
+
 export enum Colors {
   primary = '#1890ff',
 }
+
+export const LineColors: Record<keyof Indicators, string> = {
+  rsi: '#8884d8',
+  adx: '#f6bd15',
+  macd: '#5d7092',
+  macd_hist: '#5ad8a6',
+  macd_signal: '#5b8ff9',
+};
