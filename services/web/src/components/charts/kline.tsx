0055eefcb395cc425104e7309853120119d0c85e
Karan Pratap Singh <synester.gates4@gmail.com>
2022-01-31 18:26:47 +0530

    update: kline chart

--- services/web/src/components/charts/kline.tsx
+++ services/web/src/components/charts/kline.tsx
@@ -1,27 +1,61 @@
 import { Chart, dispose, init, KLineData } from 'klinecharts';
-import { map } from 'lodash';
+import { difference, map } from 'lodash';
 import React, { useEffect } from 'react';
 import { useDataFrame } from '../../store/dataframe';
 
 const CHART_ID = 'kline-chart';
 
-export function KlineChart(): React.ReactElement {
-  useEffect(() => {
-    const chart: Chart | null = init(CHART_ID, {
-      candle: {
-        tooltip: {
-          labels: ['T: ', 'O: ', 'C: ', 'H: ', 'O: ', 'V: '],
-        },
+export enum TechnicalIndicators {
+  RSI = 'RSI',
+  MACD = 'MACD',
+  VOL = 'VOL',
+
+  MA = 'MA',
+  EMA = 'EMA',
+  SMA = 'SMA',
+
+  BOLL = 'BOLL',
+  SAR = 'SAR',
+  BBI = 'BBI',
+
+  KDJ = 'KDJ',
+  OBV = 'OBV',
+}
+
+export enum ChartType {
+  CANDLE = 'candle_solid',
+  AREA = 'area',
+}
+
+interface KlineChartProps {
+  type: ChartType;
+  main: TechnicalIndicators[];
+  sub: TechnicalIndicators[];
+}
+
+const options = {
+  candle: {
+    tooltip: {
+      labels: ['T: ', 'O: ', 'C: ', 'H: ', 'O: ', 'V: '],
+    },
+  },
+  technicalIndicator: {
+    lastValueMark: {
+      show: true,
+      text: {
+        show: true,
       },
-      technicalIndicator: {
-        lastValueMark: {
-          show: true,
-          text: {
-            show: true
-          }
-        }
-      }
-    });
+    },
+  },
+};
+
+export function KlineChart(props: KlineChartProps): React.ReactElement {
+  const { type, main, sub } = props;
+
+  const [chart, setChart] = React.useState<Chart | null>(null);
+
+  useEffect(() => {
+    const chart = init(CHART_ID, options);
 
     const unsubscribe = useDataFrame.subscribe(({ data }) => {
       const klineData: KLineData[] = map(
@@ -38,11 +72,48 @@ export function KlineChart(): React.ReactElement {
       chart?.applyNewData(klineData);
     });
 
+    setChart(chart);
+
     return () => {
       dispose(CHART_ID);
       unsubscribe();
     };
   }, []);
 
-  return <div id={CHART_ID} style={{ height: 600 }}></div>;
+  useEffect(() => {
+    const options = {
+      candle: {
+        type,
+      },
+    };
+
+    chart?.setStyleOptions(options);
+  }, [chart, type]);
+
+  useEffect(() => {
+    const getSubId = (type: string) => `sub-${type}`;
+    const getMainId = () => 'candle_pane';
+
+    setIndicators(main, true, getMainId);
+    setIndicators(sub, false, getSubId);
+  }, [chart, main, sub]);
+
+  function setIndicators(
+    indicators: string[],
+    stack: boolean,
+    getId: (type: string) => string
+  ): void {
+    const all = Object.keys(TechnicalIndicators);
+    const diff = difference(all, indicators);
+
+    diff.forEach(type => {
+      chart?.removeTechnicalIndicator(getId(type));
+    });
+
+    indicators.forEach(type => {
+      chart?.createTechnicalIndicator(type, stack, { id: getId(type) });
+    });
+  }
+
+  return <div id={CHART_ID} style={{ height: '90%', width: '100%' }}></div>;
 }
