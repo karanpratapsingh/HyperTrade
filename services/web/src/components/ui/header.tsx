c027c0f7786f7eedfa075323fe5d1d81771151ba
Karan Pratap Singh <synester.gates4@gmail.com>
2022-01-31 18:26:10 +0530

    update: header

--- services/web/src/components/ui/header.tsx
+++ services/web/src/components/ui/header.tsx
@@ -9,7 +9,7 @@ export function Header(props: HeaderProps) {
   return (
     <div className='flex flex-col pb-4'>
       <span className='font-bold text-xl'>{title}</span>
-      {subtitle && <span className='italic text-gray-400'>{subtitle}</span>}
+      {subtitle && <span className='text-xs font-light italic text-gray-400'>{subtitle}</span>}
     </div>
   );
 }
