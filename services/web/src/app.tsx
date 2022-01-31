a9b1178ade0481c65064c6f4a52a45acd5ae185d
Karan Pratap Singh <synester.gates4@gmail.com>
2022-01-31 18:27:07 +0530

    update: app

--- services/web/src/app.tsx
+++ services/web/src/app.tsx
@@ -1,22 +1,23 @@
+import { Layout, Menu } from 'antd';
 import 'antd/dist/antd.css';
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import ReactDOM from 'react-dom';
+import { AiOutlineAreaChart, AiTwotonePieChart } from 'react-icons/ai';
 import { QueryClient, QueryClientProvider } from 'react-query';
-import { BalanceList } from './components/balance';
-import { IndicatorChart } from './components/charts/indicator';
-import { KlineChart } from './components/charts/kline';
 import { ErrorBoundary } from './components/error-boundary';
-import { PositionsList } from './components/positions';
-import { TradesList } from './components/trades';
-import { Header } from './components/ui/header';
 import { PubSub } from './events/pubsub';
 import { DataFrameEvent, DataFrameEventPayload } from './events/types';
+import { Chart } from './pages/chart';
 import { useDataFrame } from './store/dataframe';
 import './styles/app.css';
 
 const client = new QueryClient();
 
+const {  Sider } = Layout;
+
 function App(): React.ReactElement {
+  const [collapsed, setCollapsed] = useState(true);
+
   const add = useDataFrame(state => state.add);
 
   async function init() {
@@ -33,34 +34,21 @@ function App(): React.ReactElement {
   }, []);
 
   return (
-    <div className='h-full p-16 flex flex-1'>
-      <div className='flex flex-col mr-8 overflow-y-scroll' style={{ flex: 2 }}>
-        <div className='flex flex-col'>
-          <Header title='Candlestick' subtitle='Live data charts' />
-          <KlineChart />
-        </div>
-        <div className='flex flex-col mt-4'>
-          <Header title='Indicators' subtitle='Financial indicators' />
-          <div className='flex flex-wrap justify-between'>
-            <IndicatorChart types={['rsi']} />
-            <IndicatorChart types={['adx']} />
-            <IndicatorChart types={['macd']} />
-            <IndicatorChart types={['macd_hist', 'macd_signal']} />
-          </div>
-        </div>
-      </div>
-      <div className='flex flex-1 flex-col'>
-        <div className='flex flex-1'>
-          <BalanceList />
-        </div>
-        <div className='flex flex-1'>
-          <TradesList />
-        </div>
-        <div className='flex flex-1'>
-          <PositionsList />
-        </div>
-      </div>
-    </div>
+    <Layout className='min-h-screen'>
+      <Sider
+        collapsible
+        collapsed={collapsed}
+        onCollapse={() => setCollapsed(!collapsed)}>
+        <div className='logo' />
+        <Menu theme='dark' defaultSelectedKeys={['1']} mode='inline'>
+          <Menu.Item icon={<AiOutlineAreaChart size={20} />}>Trade</Menu.Item>
+          <Menu.Item icon={<AiTwotonePieChart size={20} />}>Info</Menu.Item>
+        </Menu>
+      </Sider>
+      <Layout>
+        <Chart />
+      </Layout>
+    </Layout>
   );
 }
 
