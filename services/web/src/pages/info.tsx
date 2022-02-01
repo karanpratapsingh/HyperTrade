90d084bac66c4a0ae0d66d323c18f3ebea86e578
Karan Pratap Singh <synester.gates4@gmail.com>
2022-02-01 19:00:09 +0530

    feat: update info

--- services/web/src/pages/info.tsx
+++ services/web/src/pages/info.tsx
@@ -1,4 +1,5 @@
-import { Layout } from 'antd';
+import { Divider, Layout } from 'antd';
+import { TradeChart } from '../components/charts/trade';
 import { BalanceTable } from '../components/tables/balance';
 import { PositionsTable } from '../components/tables/positions';
 import { TradesTable } from '../components/tables/trades';
@@ -8,16 +9,15 @@ const { Content } = Layout;
 export function Info(): React.ReactElement {
   return (
     <Content className='flex pl-6 pt-6 p-4 bg-white'>
-      <div className='flex mr-4' style={{ flex: 3 }}>
-        <div className='flex flex-1'>
-          <TradesTable />
-        </div>
-        <div className='flex flex-1'></div>
-      </div>
       <div className='flex flex-col' style={{ flex: 2 }}>
         <BalanceTable />
         <PositionsTable />
         <TradesTable />
+      </div>
+      <Divider className='min-h-full mx-12' type='vertical' />
+      <div className='flex flex-col' style={{ flex: 3 }}>
+        <div className='flex flex-1'></div>
+        <TradeChart />
       </div>
     </Content>
   );
