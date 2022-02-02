986d3ea0044387155936bbf20644e404c449145c
Karan Pratap Singh <synester.gates4@gmail.com>
2022-02-02 19:53:49 +0530

    feat: add more icons

--- services/web/src/theme/icons.ts
+++ services/web/src/theme/icons.ts
@@ -1,5 +1,17 @@
 import usdt from 'cryptocurrency-icons/svg/icon/usdt.svg';
+import bnb from 'cryptocurrency-icons/svg/icon/bnb.svg';
+import btc from 'cryptocurrency-icons/svg/icon/btc.svg';
+import eth from 'cryptocurrency-icons/svg/icon/eth.svg';
+import ltc from 'cryptocurrency-icons/svg/icon/ltc.svg';
+import trx from 'cryptocurrency-icons/svg/icon/trx.svg';
+import xrp from 'cryptocurrency-icons/svg/icon/bnb.svg';
 
 export const Icons: Record<string, string> = {
   usdt,
+  bnb,
+  btc,
+  eth,
+  ltc,
+  trx,
+  xrp,
 };
