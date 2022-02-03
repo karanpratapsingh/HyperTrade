0c5193798e8acf32b0e320343964789f1225c9a1
Karan Pratap Singh <synester.gates4@gmail.com>
2022-02-03 19:15:00 +0530

    feat: web connect stats api

---
+++ services/web/src/api/stats.ts
@@ -0,0 +1,26 @@
+import axios, { AxiosResponse } from 'axios';
+import { useQuery } from 'react-query';
+import { ApiHookResult } from './types';
+
+export type Stats = {
+  profit: number;
+  loss: number;
+  total: number;
+};
+
+export type StatsResponse = {
+  stats: Stats | null;
+};
+
+export function useStats(): ApiHookResult<StatsResponse> {
+  const fetch = () => axios.get('/exchange/stats');
+  const {
+    data,
+    isLoading: loading,
+    error,
+  } = useQuery<AxiosResponse<StatsResponse, Error>, Error>('stats', fetch, {
+    refetchInterval: 4 * 1000,
+  });
+
+  return { data: data?.data, loading, error };
+}
