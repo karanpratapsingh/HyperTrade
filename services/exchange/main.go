932e7c0279aa8c96bd123941c3aed82383b55a84
Karan Pratap Singh <synester.gates4@gmail.com>
2022-02-06 00:23:53 +0530

    feat: simplify main

--- services/exchange/main.go
+++ services/exchange/main.go
@@ -7,7 +7,6 @@ import (
 	"exchange/internal"
 	"exchange/utils"
 
-	"github.com/adshao/go-binance/v2"
 	"github.com/rs/zerolog"
 	"github.com/rs/zerolog/log"
 )
@@ -33,73 +32,5 @@ func main() {
 
 	go bex.Kline(symbol, interval)
 
-	pubsub.Subscribe(internal.DataFrameEvent, func(p internal.DataFrameEventPayload) {
-		ListenTrade(DB, pubsub, p.Kline, p.Signal)
-	})
-
-	err := internal.NewApi(DB, bex, pubsub)
-	log.Error().Err(err).Msg("Router.Error")
-}
-
-// TODO: refactor this
-func ListenTrade(DB db.DB, pubsub internal.PubSub, kline internal.Kline, signal internal.Signal) {
-	side := getSide(signal)
-
-	if side == "" {
-		return
-	}
-
-	symbol := kline.Symbol
-
-	log.Trace().Str("symbol", symbol).Interface("side", side).Msg("Trade.Listen")
-
-	position := DB.GetPosition(symbol)
-	var holding bool = position.Symbol != ""
-
-	config := DB.GetConfig(symbol)
-
-	allowedAmt := config.AllowedAmount
-	closePrice := kline.Close
-	quantity := internal.GetMinQuantity(allowedAmt, closePrice)
-
-	switch side {
-	case binance.SideTypeBuy:
-		if holding {
-			log.Warn().Bool("holding", holding).Msg("Trade.Buy.Skip")
-			return
-		}
-
-		// TODO: Execute Buy
-		DB.CreatePosition(symbol, closePrice, quantity)
-		log.Trace().Float64("price", closePrice).Float64("quantity", quantity).Msg("Trade.Buy.Complete")
-
-	case binance.SideTypeSell:
-		if !holding {
-			log.Warn().Bool("holding", holding).Msg("Trade.Sell.Skip")
-			return
-		}
-
-		// TODO: Execute Sell
-		entry := position.Price
-		DB.DeletePosition(symbol)
-		trade := DB.CreateTrade(symbol, entry, closePrice, quantity)
-
-		payload := internal.TradeEventPayload{trade.ID, trade.Symbol, trade.Entry, trade.Exit, trade.Quantity, trade.Time}
-		pubsub.Publish(internal.TradeEvent, payload)
-
-		log.Trace().Float64("price", closePrice).Float64("quantity", quantity).Msg("Trade.Sell.Complete")
-	default:
-	}
-}
-
-func getSide(signal internal.Signal) binance.SideType {
-	var side binance.SideType
-
-	if signal.Buy && !signal.Sell {
-		side = binance.SideTypeBuy
-	} else if signal.Sell && !signal.Buy {
-		side = binance.SideTypeSell
-	}
-
-	return side
+	internal.RunAsyncApi(DB, bex, pubsub)
 }
