afda4ae322686c2bddffe64125839e0bb6b10e58
Karan Pratap Singh <synester.gates4@gmail.com>
2022-02-07 23:50:42 +0530

    update: utils pagination

--- services/web/src/utils/pagination.ts
+++ services/web/src/utils/pagination.ts
@@ -2,9 +2,12 @@ import { TablePaginationConfig } from 'antd';
 
 const PAGE_SIZE = 4;
 
-export function paginationProps(length: number): TablePaginationConfig | false {
+export function paginationProps(
+  length: number,
+  pageSize?: number
+): TablePaginationConfig | false {
   if (length > PAGE_SIZE) {
-    return { pageSize: PAGE_SIZE };
+    return { pageSize: pageSize || PAGE_SIZE };
   }
 
   return false;
