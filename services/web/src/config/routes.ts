0b3950aa59ce5e4510e7fe8cfa2cb0e14ef15033
Karan Pratap Singh <synester.gates4@gmail.com>
2022-02-20 17:24:04 +0530

    update: routes

--- services/web/src/config/routes.ts
+++ services/web/src/config/routes.ts
@@ -2,10 +2,12 @@ export enum MenuItem {
   CHART = 'chart',
   DATAFRAME = 'dataframe',
   PORTFOLIO = 'portfolio',
+  CONFIG = 'config',
 }
 
 export enum Paths {
   HOME = '/',
   DATAFRAME = '/dataframe',
   PORTFOLIO = '/portfolio',
+  CONFIG = '/config',
 }
