225194751bf9a756580521d95907b59f48df3d1a
Karan Pratap Singh <synester.gates4@gmail.com>
2022-02-20 19:27:04 +0530

    feat(types): add mutation types

--- services/web/src/api/types.ts
+++ services/web/src/api/types.ts
@@ -1,3 +1,5 @@
+import { UseMutateFunction } from 'react-query';
+
 export const options = {
   refetchInterval: 5 * 1000,
 };
@@ -6,4 +8,10 @@ export interface ApiQueryResult<T, E = Error> {
   data: T | undefined;
   loading: boolean;
   error: E | null;
+}
+
+export interface ApiMutationResult<T, V, E = unknown> {
+  mutate: UseMutateFunction<T, E, V>;
+  data: T | undefined;
+  loading: boolean;
 }
