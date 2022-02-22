c2d8dfe3da5fb94e598d16d59a66f8ddb9f59cbc
Karan Pratap Singh <synester.gates4@gmail.com>
2022-02-22 13:12:03 +0530

    feat(db): macd strategy type

--- services/exchange/db/custom.types.go
+++ services/exchange/db/custom.types.go
@@ -27,3 +27,22 @@ func (r *Rsi) Scan(value interface{}) error {
 func (r Rsi) Value() (driver.Value, error) {
 	return json.Marshal(r)
 }
+
+type Macd struct {
+	CommonStrategyProps
+	Fast   int `json:"fast"`
+	Slow   int `json:"slow"`
+	Signal int `json:"signal"`
+}
+
+func (Macd) GormDataType() string {
+	return "JSONB"
+}
+
+func (r *Macd) Scan(value interface{}) error {
+	return json.Unmarshal(value.([]byte), &r)
+}
+
+func (r Macd) Value() (driver.Value, error) {
+	return json.Marshal(r)
+}
