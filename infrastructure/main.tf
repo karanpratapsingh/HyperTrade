89376561e92835d0dfc0d643d0447358d07a0402
Karan Pratap Singh <synester.gates4@gmail.com>
2022-03-17 20:38:28 +0530

    update: infrastructure

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -1,5 +1,12 @@
 locals {
-  name = "trader"
+  name = "hypertrade"
+  docker_credentials = {
+    "auths" = {
+      "ghcr.io" = {
+        "auth" = base64encode("${var.github_username}:${var.GHCR_TOKEN}")
+      }
+    }
+  }
 }
 
 resource "digitalocean_container_registry" "registry" {
@@ -10,17 +17,13 @@ resource "digitalocean_container_registry" "registry" {
 resource "digitalocean_kubernetes_cluster" "cluster" {
   name    = "${local.name}-cluster"
   region  = "nyc1"
-  version = "1.21.9-do.0"
+  version = "1.22.7-do.0"
 
   node_pool {
     name       = "${local.name}-node-pool"
     size       = "s-1vcpu-2gb"
-    node_count = 2
+    node_count = 1
   }
-}
-
-resource "digitalocean_container_registry_docker_credentials" "credentials" {
-  registry_name = digitalocean_container_registry.registry.name
 }
 
 provider "kubernetes" {
@@ -32,13 +35,13 @@ provider "kubernetes" {
 }
 
 resource "kubernetes_secret" "secret" {
+  type = "kubernetes.io/dockerconfigjson"
+
   metadata {
     name = "${local.name}-docker-config"
   }
 
   data = {
-    ".dockerconfigjson" = digitalocean_container_registry_docker_credentials.credentials.docker_credentials
+    ".dockerconfigjson" = jsonencode(local.docker_credentials)
   }
-
-  type = "kubernetes.io/dockerconfigjson"
 }
